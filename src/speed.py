1a0d9a8cf278678e8b1c75af5d143d1b6a5d3d8a
dpw6 <dpw6@sfu.ca>
2021-09-22 00:01:31 -0700

    added speed@kph function

--- src/speed.py
+++ src/speed.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 #g is the gravitational force constant
 g = 9.80655
 
@@ -22,16 +23,43 @@ def airResistance(speedKPH, grade, elevation):
         cda = 0.324
     else:
         cda = 0.307
-    ar = 0.5 * cda * airDensity(elevation) * ((v + windV)**2)
+    ar = 0.5 * cda * airDensity(elevation) * (v**2)
+    #ar = 0.5 * cda * airDensity(elevation) * ((v + windV)**2)
     return ar
 
 def airDensity(elevation):
     ad = 1.225 * math.exp(-0.00011856 * elevation)
     return ad
 
-def totalForce(grade, totalSystemWeight, speedKPH, elevation, crr):
+def powerRequired(speedKPH, totalSystemWeight, grade, elevation, crr):
     tf = (forceOfGravity(totalSystemWeight, grade) 
     + rollingResistance(totalSystemWeight, grade, crr) 
     + airResistance(speedKPH, grade, elevation))
     v = speedKPH/3.6
     return tf * v
+
+#current iteration of speedAtPower only accounts for air drag
+#rolling resistance and force of gravity are unaccounted for
+#THIS IS IMPORTANT
+def speedKPHAtPower(power, totalSystemWeight, grade, elevation, crr):
+    ad = airDensity(elevation)
+    rr = rollingResistance(totalSystemWeight, grade, crr)
+    fg = forceOfGravity(totalSystemWeight, grade)
+    
+    if grade > 2.5:
+        cda = 0.324
+    else:
+        cda = 0.307
+    
+    #equation is:
+    #(0.5*cda*ad)*v^3+(fg+rr)*v-pwr = 0
+    
+    coeff = [0.5*cda*ad, 0, fg+rr, -power]
+    print(np.roots(coeff))
+    
+    kph = 3.6
+   
+    return kph
+
+s = speedKPHAtPower(100, 80, 0, 0, 0)
+print(s)
